d33616d45b8b1456d3f3802daf39061e5f71ef7f
Shanreed25 <shannontlreed@gmail.com>
2025-06-05 20:11:25 -0700

    added css styles to the contact section

--- code/esmodules/generatepdf.js
+++ code/esmodules/generatepdf.js
@@ -5,7 +5,7 @@ export function generatePDF() {
 
   // Optional configuration
   const opt = {
-    margin: 1,
+    margin: .1,
     filename: "myfile.pdf",
     image: { type: "jpeg", quality: 0.98 },
     html2canvas: { scale: 2 },
