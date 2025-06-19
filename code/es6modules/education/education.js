7566039b0b1bd9635a9c938298f8e472e5dbafd9
Shanreed25 <shannontlreed@gmail.com>
2025-06-18 23:28:40 -0700

    adding education school name and dates inputs

--- code/es6modules/education/education.js
+++ code/es6modules/education/education.js
@@ -5,17 +5,27 @@ import { addEducationPreviewContainer} from './educationPreview.js';
 export default function addEducation(educationFormCount){
     //Add education form container
     const educationFormContainer = addEducationFormContainer(educationFormCount);
-    const educationDegreeInput = educationFormContainer.querySelector(`#education-school-degree-input-${educationFormCount}`)
-    
+    //Get The Degree Input
+    const degreeInput = educationFormContainer.querySelector(`#degree-input-${educationFormCount}`)
+
+    const schoolNameInput = educationFormContainer.querySelector(`#school-name-input-${educationFormCount}`)
     
     //Add education preview container
     const educationPreviewContainer = addEducationPreviewContainer(educationFormCount);
+    //Get The Degree h4 element
     const educationDegreeH4 = educationPreviewContainer.querySelector(`#school-info-preview-container-${educationFormCount}`)
-    console.log(educationFormContainer, educationPreviewContainer )
+    
+    console.log(educationFormContainer, educationPreviewContainer);
 
-     // ---> Listen for the input and add the value to the preview
-     educationDegreeInput.addEventListener('input', () => {
-        educationDegreeH4.textContent = educationDegreeInput.value
+     // ---> Listen for the degree input and add the value to the preview
+     degreeInput.addEventListener('input', () => {
+        educationDegreeH4.textContent = degreeInput.value;
+     })
+
+
+      // ---> Listen for the input and add the value to the preview
+     schoolNameInput.addEventListener('input', () => {
+        educationDegreeH4.textContent = degreeInput.value;
      })
 
 }
--- code/es6modules/education/educationInputs.js
+++ code/es6modules/education/educationInputs.js
@@ -3,15 +3,45 @@ const allEducationFormsContainer = document.getElementById('all-education-forms-
 //Container and Inputs For School Name nad Degree
 export function addEducationFormContainer(educationFormCount){
     //Container for each education Form
-    const educationSchoolInfoContainer = document.createElement('div')
-    educationSchoolInfoContainer.id = `education-school-info-container-${educationFormCount}`;
-    educationSchoolInfoContainer.className = 'education-school-info-container';
+    const educationFormContainer = document.createElement('div')
+    educationFormContainer.id = `education-form-container-${educationFormCount}`;
+    educationFormContainer.className = 'education-form-container';
 
+    //Container for each degree input------------------------------------------------------------------
+    const degreeInputContainer = document.createElement('div')
+    degreeInputContainer.id = `degree-input-container-${educationFormCount}`;
+    degreeInputContainer.className = 'degree-input-container';
 
-    // School Degree Input
-    const educationFormSchoolDegreeInput = document.createElement('input')
-    educationFormSchoolDegreeInput.id = `education-school-degree-input-${educationFormCount}`;
-    educationFormSchoolDegreeInput.className = 'education-school-degree-input';
+    // Degree Input
+    const degreeInput = document.createElement('input')
+    degreeInput.id = `degree-input-${educationFormCount}`;
+    degreeInput.className = 'degree-input';
+
+     //Container for school name and dates inputs------------------------------------------------------------------
+    const schoolNameDateInputContainer = document.createElement('div')
+    schoolNameDateInputContainer.id = `school-name-date-input-container-${educationFormCount}`;
+    schoolNameDateInputContainer.className = 'school-name-date-input-container';
+
+    // School Name Input
+    const schoolNameInput = document.createElement('input')
+    schoolNameInput.id = `school-name-input-${educationFormCount}`;
+    schoolNameInput.className = 'school-name-input';
+
+    const schoolDatesInputContainer = document.createElement('div')
+    schoolDatesInputContainer.id = `school-dates-input-container-${educationFormCount}`;
+    schoolDatesInputContainer.className = 'school-dates-input-container';
+    
+    // School Start Date Input
+    const schoolStartDateInput = document.createElement('input')
+    schoolStartDateInput.id = `school-start-date-input-${educationFormCount}`;
+    schoolStartDateInput.className = 'school-start-date-input';
+    schoolStartDateInput.type = 'month';
+
+     // School Start Date Input
+    const schoolEndDateInput = document.createElement('input')
+    schoolEndDateInput.id = `school-end-date-input-${educationFormCount}`;
+    schoolEndDateInput.className = 'school-end-date-input';
+    schoolEndDateInput.type = 'month';
 
     // //School Name Input
     // const educationFormSchoolNameInput = document.createElement('input')
@@ -30,10 +60,18 @@ export function addEducationFormContainer(educationFormCount){
     // educationFormSchoolDegreeInput.id = `education-school-degree-input-${educationFormCount}`;
     // educationFormSchoolDegreeInput.className = 'education-school-degree-input';
 
-    educationSchoolInfoContainer.append( educationFormSchoolDegreeInput);
-    allEducationFormsContainer.append(educationSchoolInfoContainer)
+
+    // Education Form Container with Input Containers
+    educationFormContainer.append(degreeInputContainer, schoolNameDateInputContainer);
+    //Degree Input Container with degree Input
+    degreeInputContainer.append(degreeInput);
+
+    //School Name and Dates Input Container with school name input and school dates container
+    schoolNameDateInputContainer.append(schoolNameInput, schoolDatesInputContainer);
+    schoolDatesInputContainer.append(schoolStartDateInput, schoolEndDateInput )
+    allEducationFormsContainer.append(educationFormContainer);
     
-    return educationSchoolInfoContainer;
+    return educationFormContainer;
 }
 
 
