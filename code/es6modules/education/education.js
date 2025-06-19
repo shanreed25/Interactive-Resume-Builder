8433b46f5293670936e1195f7d3bf7fbc01bec36
Shanreed25 <shannontlreed@gmail.com>
2025-06-19 00:46:27 -0700

    added education inputs and preview for degree, school name and dates

--- code/es6modules/education/education.js
+++ code/es6modules/education/education.js
@@ -3,29 +3,49 @@ import { addEducationPreviewContainer} from './educationPreview.js';
 
 
 export default function addEducation(educationFormCount){
-    //Add education form container
+    //Add education form container-------------------------------------------------------------------------------->
     const educationFormContainer = addEducationFormContainer(educationFormCount);
     //Get The Degree Input
-    const degreeInput = educationFormContainer.querySelector(`#degree-input-${educationFormCount}`)
+    const degreeInput = educationFormContainer.querySelector(`#degree-input-${educationFormCount}`);
 
-    const schoolNameInput = educationFormContainer.querySelector(`#school-name-input-${educationFormCount}`)
+    const schoolNameInput = educationFormContainer.querySelector(`#school-name-input-${educationFormCount}`);
     
-    //Add education preview container
+    const schoolStartDateInput = educationFormContainer.querySelector(`#school-start-date-input-${educationFormCount}`);
+    
+    const schoolEndDateInput = educationFormContainer.querySelector(`#school-end-date-input-${educationFormCount}`);
+    
+    //Add education preview container----------------------------------------------------------------------------------------------->
     const educationPreviewContainer = addEducationPreviewContainer(educationFormCount);
     //Get The Degree h4 element
-    const educationDegreeH4 = educationPreviewContainer.querySelector(`#school-info-preview-container-${educationFormCount}`)
+    const educationDegreeH4 = educationPreviewContainer.querySelector(`#school-info-preview-container-${educationFormCount}`);
     
+    //Get The School Name h5 element
+    const schoolNameH5 = educationPreviewContainer.querySelector(`#school-name-input-preview-${educationFormCount}`);
+
+    //Get The School Start Date h5 element
+    const schoolStartDateH5 = educationPreviewContainer.querySelector(`#school-start-date-input-preview-${educationFormCount}`);
+
+     //Get The School Name h5 element
+    const schoolEndDateH5 = educationPreviewContainer.querySelector(`#school-end-date-input-preview-${educationFormCount}`);
     console.log(educationFormContainer, educationPreviewContainer);
 
      // ---> Listen for the degree input and add the value to the preview
      degreeInput.addEventListener('input', () => {
         educationDegreeH4.textContent = degreeInput.value;
-     })
+     });
 
 
       // ---> Listen for the input and add the value to the preview
      schoolNameInput.addEventListener('input', () => {
-        educationDegreeH4.textContent = degreeInput.value;
+        schoolNameH5.textContent = schoolNameInput.value;
+     });
+
+     schoolStartDateInput.addEventListener('input', () => {
+         schoolStartDateH5.textContent = schoolStartDateInput.value;
+     });
+
+     schoolEndDateInput.addEventListener('input', () => {
+         schoolEndDateH5.textContent = schoolEndDateInput.value;
      })
 
 }
--- code/es6modules/education/educationPreview.js
+++ code/es6modules/education/educationPreview.js
@@ -1,36 +1,55 @@
 const allEducationPreviewContainer = document.getElementById('all-education-preview-container');
 
 export function addEducationPreviewContainer(educationFormCount){
-    //Container for each education review
+    //Container for each education preview
     const educationPreviewContainer = document.createElement("div");
     educationPreviewContainer.id = `education-preview-container-${educationFormCount}`;
     educationPreviewContainer.className = `education-preview-container`;
 
-    //Container for School Degree Input
+    //Container for School Degree Input Preview
     const schoolDegreePreviewContainer = document.createElement("div");
-    schoolDegreePreviewContainer .id = `school-degree-preview-container-${educationFormCount}`;
-    schoolDegreePreviewContainer .className = `school-degree-preview-container`;
+    schoolDegreePreviewContainer.id = `school-degree-preview-container-${educationFormCount}`;
+    schoolDegreePreviewContainer.className = `school-degree-preview-container`;
 
-    //School Degree Input Review
+    //School Degree Input Preview
     const schoolDegreeInputPreview = document.createElement("h4");
     schoolDegreeInputPreview.id = `school-info-preview-container-${educationFormCount}`;
     schoolDegreeInputPreview.className = `school-info-preview-container`;
 
 
-    //Container for School Name and date Input
+    //Container for School Name and dates Input Preview
     const schoolNameDatesPreviewContainer = document.createElement("div");
-    schoolNameDatesPreviewContainer .id = `school-name-dates-preview-container-${educationFormCount}`;
-    schoolNameDatesPreviewContainer .className = `school-name-dates-preview-container`;
+    schoolNameDatesPreviewContainer.id = `school-name-dates-preview-container-${educationFormCount}`;
+    schoolNameDatesPreviewContainer.className = `school-name-dates-preview-container`;
 
     //School Name Preview
     const schoolNameInputPreview = document.createElement("h5");
-    schoolNameInputPreview .id = `school-name-dates-preview-container-${educationFormCount}`;
-    schoolNameInputPreview .className = `school-name-dates-preview-container`;
+    schoolNameInputPreview.id = `school-name-input-preview-${educationFormCount}`;
+    schoolNameInputPreview.className = `school-name-input-preview`;
+
+    //Container for School Dates Input Preview
+    const schoolDatesPreviewContainer = document.createElement("div");
+    schoolDatesPreviewContainer.id = `school-dates-preview-container-${educationFormCount}`;
+    schoolDatesPreviewContainer.className = `school-dates-preview-container`;
+
+     //School Start Date Preview
+    const schoolStartDateInputPreview = document.createElement("h5");
+    schoolStartDateInputPreview.id = `school-start-date-input-preview-${educationFormCount}`;
+    schoolStartDateInputPreview.className = `school-start-date-input-preview`;
+
+     //School End Date Preview
+    const schoolEndDateInputPreview = document.createElement("h5");
+    schoolEndDateInputPreview.id = `school-end-date-input-preview-${educationFormCount}`;
+    schoolEndDateInputPreview.className = `school-end-date-input-preview`;
+
+    
 
 
     allEducationPreviewContainer.append(educationPreviewContainer);
-    educationPreviewContainer.append(schoolDegreePreviewContainer);
+    educationPreviewContainer.append(schoolDegreePreviewContainer, schoolNameDatesPreviewContainer);
     schoolDegreePreviewContainer.append(schoolDegreeInputPreview);
+    schoolNameDatesPreviewContainer .append(schoolNameInputPreview, schoolDatesPreviewContainer);
+    schoolDatesPreviewContainer.append(schoolStartDateInputPreview, schoolEndDateInputPreview)
     
     return educationPreviewContainer;
 
